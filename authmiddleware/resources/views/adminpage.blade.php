beb625e34d8a37fc4657a998eb9e2e5004fd27a5
loljaar <vaghelajatin5@example.com>
2023-12-21 12:05:21 +0530

    learning middleware

---
+++ authmiddleware/resources/views/adminpage.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+</head>
+<body>
+    <div>
+        <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
+        <h1>only admin can come</h1>
+    </div>
+</body>
+</html>
+
